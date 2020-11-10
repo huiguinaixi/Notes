a02f4e905504e2adfb338a1bbdcf4563cf216429
25447 <wei.li@sipingsoft.com>
2020-11-10 10:16:48 +0800

    增加一个方法

--- notes.js
+++ notes.js
@@ -1,1 +1,23 @@
-// 文件上传
+/**
+ * ????????
+ * url ?????
+ * limitTime ?????**/
+let padding = []
+const storagePadding = function (url, limitTime) {
+    const curTime = +new Date()
+    for (let index = 0; index < padding.length; index++) {
+        if (padding[index].url === url) {
+            let flag = false
+            if (curTime - padding[index].datatime < limitTime) {
+                flag = true
+            }
+            padding[index].datatime = curTime
+            return flag
+        }
+    }
+    padding.push({
+        url,
+        datatime: +new Date()
+    })
+    return false
+}
